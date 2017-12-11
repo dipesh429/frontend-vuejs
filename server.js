ed13e7762713e9037fa85c451c633b4f3cb640e0
Dipesh <kcdipesh429@gmail.com>
2017-12-11 23:02:03 +0545

    aba ta vayo hola niii

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
+
+app = express();
+
+var history = require('connect-history-api-fallback');
+
+app.use(history({
+  // verbose: true
+}));
+
 var path = require('path');
 var serveStatic = require('serve-static');
-app = express();
+
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
